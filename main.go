0375fa2480e2f2c5a6d89a6a5502a5a17b8f4d04
Matt Anger <AngerMCS@gmail.com>
2021-08-01 14:35:53 -0700

    switch to gin

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	json "github.com/json-iterator/go"
 )
 
@@ -19,35 +19,30 @@ type req struct {
 	Query      url.Values
 }
 
-type echoServer struct{}
-
-func (e echoServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	body, _ := ioutil.ReadAll(r.Body)
+func ServeHTTP(c *gin.Context) {
+	defer c.Request.Body.Close()
+	body, _ := ioutil.ReadAll(c.Request.Body)
 	resp := req{
-		Method:  r.Method,
-		Headers: r.Header,
+		Method:  c.Request.Method,
+		Headers: c.Request.Header,
 		Body:    string(body),
-		URL:     r.URL,
-		Query:   r.URL.Query(),
+		URL:     c.Request.URL,
+		Query:   c.Request.URL.Query(),
 	}
 	// Parse body if json
 	json.Unmarshal(body, &resp.ParsedBody)
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Cache-Control", "no-cache")
+	c.Header("Content-Type", "application/json")
+	c.Header("Cache-Control", "no-cache")
 	buf, _ := json.Marshal(resp)
-	w.Write(buf)
+	c.Writer.Write(buf)
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = "8080"
 	}
-	myHandler := &echoServer{}
-	s := http.Server{
-		Addr:    ":" + port,
-		Handler: myHandler,
-	}
-	s.ListenAndServe()
+	s := gin.Default()
+	s.Any("", ServeHTTP)
+	s.Run(":" + port)
 }
